5d599bc54644ba31721218700b522ce8ae328b58
ramvasi <ramvasi@outlook.com>
2021-10-13 23:55:12 +0530

    update SPDX header

--- cidr_management/integ_test/environment.py
+++ cidr_management/integ_test/environment.py
@@ -1,3 +1,6 @@
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: MIT-0
+
 # pylint: skip-file
 """Set up proper configurations for CIDR integration"""
 import json
